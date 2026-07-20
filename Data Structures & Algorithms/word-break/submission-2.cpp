47f6b969841543428904d6a486c1f283b5c5eda9
Ashish Purohit <115914472+Ashishpur0hit@users.noreply.github.com>
2026-07-20 23:00:44 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool dfs(string &s,vector<string>& wordDict,int index,vector<int>&dp)
+    {
+        if(index==s.length()) return true;
+        if(dp[index]!=-1) return dp[index];
+        for(auto x : wordDict)
+        {
+            if(index+x.length()<=s.length() && s.substr(index,x.length())==x)
+            {
+                if(dfs(s,wordDict,index+x.length(),dp)) 
+                {
+                    dp[index]=1;
+                    return true;
+                }
+            }
+        }
+        dp[index]=0;
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.length(),-1);
+        return dfs(s,wordDict,0,dp);
+    }
+};
